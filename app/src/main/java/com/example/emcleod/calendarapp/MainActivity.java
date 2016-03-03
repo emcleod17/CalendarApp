1c0fed57edd97fd7c4febbc0d76b73b8220e9c70
emcleod17 <emcleod@wisc.edu>
2016-03-03 13:51:31 -0600

    -Began implementing the main activity calendar view

--- app/src/main/java/com/example/emcleod/calendarapp/MainActivity.java
+++ app/src/main/java/com/example/emcleod/calendarapp/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.emcleod.calendarapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.CalendarView.OnDateChangeListener;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    CalendarView calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeCalendar();
+    }
+
+    public void initializeCalendar() {
+        calendar = (CalendarView) findViewById(R.id.calendarView);
+
+        calendar.setShowWeekNumber(false);
+        calendar.setFirstDayOfWeek(1);
+        calendar.setUnfocusedMonthDateColor(getResources().getColor(R.color.transparent));
+        calendar.setSelectedWeekBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        calendar.setWeekSeparatorLineColor(getResources().getColor(R.color.transparent));
+        calendar.setSelectedDateVerticalBar(R.color.colorAccent);
+
+        calendar.setOnDateChangeListener(new OnDateChangeListener() {
+                    //show the selected date as a toast
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
+                Toast.makeText(getApplicationContext(), day + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
